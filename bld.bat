6d3c02c3b67bb4f94c7328bbf8af1dd1cd09a18e
Christian Fobel <christian@fobel.net>
2018-02-22 09:02:31 -0500

    fix(py3): patch imports nanopb_generator for Py 3

--- bld.bat
+++ bld.bat
@@ -1,34 +1,24 @@
-set NANO_SRC=nanopb-0.3.7
-set NANO_SRC_DIR=nanopb
+@echo on
+set MODULE_DIR=%SP_DIR%\nanopb_generator
 
-REM Download Windows (32-bit) binary.
-"%PYTHON%" -m wget https://koti.kapsi.fi/~jpa/nanopb/download/%NANO_SRC%.tar.gz
+REM Patch generator to support Python 3
+patch --verbose --binary -l generator/nanopb_generator.py -i "%RECIPE_DIR%\nanopb_generator.py3-support.patch"
 if errorlevel 1 exit 1
-
-REM Extract release from ZIP archive.
-"%PREFIX%"\Library\bin\7za x %NANO_SRC%.tar.gz -y
-"%PREFIX%"\Library\bin\7za x %NANO_SRC%.tar -y
-if errorlevel 1 exit 1
-
 REM Convert `nanopb_generator.py` to Python module
-md nanopb_generator
+md "%MODULE_DIR%"
 if errorlevel 1 exit 1
-copy "%NANO_SRC_DIR%"\generator\nanopb_generator.py nanopb_generator\__main__.py
+copy generator\nanopb_generator.py "%MODULE_DIR%\__main__.py"
 if errorlevel 1 exit 1
 echo "" > nanopb_generator\__init__.py
 if errorlevel 1 exit 1
 
 REM Generate nanopb Python protobuf definitions.
-set NANO_PROTO_DIR="%NANO_SRC_DIR%"\generator\proto
+set NANO_PROTO_DIR=generator\proto
 protoc --python_out=%NANO_PROTO_DIR% --proto_path=%NANO_PROTO_DIR% %NANO_PROTO_DIR%\nanopb.proto
 if errorlevel 1 exit 1
 protoc --python_out=%NANO_PROTO_DIR% --proto_path=%NANO_PROTO_DIR% %NANO_PROTO_DIR%\plugin.proto
 if errorlevel 1 exit 1
-xcopy /S /Y /I /Q %NANO_PROTO_DIR% nanopb_generator\proto
-if errorlevel 1 exit 1
-
-REM Copy `nanopb_generator` Python module to site-packages
-xcopy /S /Y /I /Q nanopb_generator "%PREFIX%"\Lib\site-packages\nanopb_generator
+xcopy /S /Y /I /Q %NANO_PROTO_DIR% "%MODULE_DIR%\proto"
 if errorlevel 1 exit 1
 
 REM Create batch file to run `nanopb_generator` Python module as a script
@@ -47,10 +37,10 @@ md "%PREFIX%"\Library\include\Arduino\nanopb\src
 endlocal
 copy "%RECIPE_DIR%"\library.properties "%PREFIX%"\Library\include\Arduino\nanopb
 copy "%RECIPE_DIR%"\nanopb.h "%PREFIX%"\Library\include\Arduino\nanopb\src
-copy "%NANO_SRC_DIR%"\*.h "%PREFIX%"\Library\include\Arduino\nanopb\src
-copy "%NANO_SRC_DIR%"\*.c "%PREFIX%"\Library\include\Arduino\nanopb\src
-copy "%NANO_SRC_DIR%"\*.txt "%PREFIX%"\Library\include\Arduino\nanopb\src
-copy "%NANO_SRC_DIR%"\*.md "%PREFIX%"\Library\include\Arduino\nanopb\src
-copy "%NANO_SRC_DIR%"\AUTHORS "%PREFIX%"\Library\include\Arduino\nanopb\src
-copy "%NANO_SRC_DIR%"\BUILD "%PREFIX%"\Library\include\Arduino\nanopb\src
-copy "%NANO_SRC_DIR%"\CHANGELOG.txt "%PREFIX%"\Library\include\Arduino\nanopb\src
+copy *.h "%PREFIX%"\Library\include\Arduino\nanopb\src
+copy *.c "%PREFIX%"\Library\include\Arduino\nanopb\src
+copy *.txt "%PREFIX%"\Library\include\Arduino\nanopb\src
+copy *.md "%PREFIX%"\Library\include\Arduino\nanopb\src
+copy AUTHORS "%PREFIX%"\Library\include\Arduino\nanopb\src
+copy BUILD "%PREFIX%"\Library\include\Arduino\nanopb\src
+copy CHANGELOG.txt "%PREFIX%"\Library\include\Arduino\nanopb\src
